ebafad9700f5f256f45ae306346f00a116a33aa7
Julian Briones <118573467+cedricJB07@users.noreply.github.com>
2023-10-14 19:01:07 -0700

    Update ProjectDatabase.java

--- TermProjectAssignmentStart/src/application/database/ProjectDatabase.java
+++ TermProjectAssignmentStart/src/application/database/ProjectDatabase.java
@@ -18,52 +18,9 @@ public class ProjectDatabase {
         }
     }
 
-    private void createProjectTableIfNotExists() {
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS projects (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "name TEXT NOT NULL, " +
-                "start_date TEXT NOT NULL, " +
-                "description TEXT)";
-        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
-            statement.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    
 
-    public void insertProject(Project project) {
-        String insertSQL = "INSERT INTO projects (name, start_date, description) VALUES (?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(insertSQL)) {
-            statement.setString(1, project.getName());
-            statement.setString(2, project.getStartDate());
-            statement.setString(3, project.getDescription());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void updateProject(Project project) {
-        String updateSQL = "UPDATE projects SET start_date = ?, description = ? WHERE name = ?";
-        try (PreparedStatement statement = connection.prepareStatement(updateSQL)) {
-            statement.setString(1, project.getStartDate());
-            statement.setString(2, project.getDescription());
-            statement.setString(3, project.getName());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void deleteProject(String projectName) {
-        String deleteSQL = "DELETE FROM projects WHERE name = ?";
-        try (PreparedStatement statement = connection.prepareStatement(deleteSQL)) {
-            statement.setString(1, projectName);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    
 
     public void close() {
         try {
