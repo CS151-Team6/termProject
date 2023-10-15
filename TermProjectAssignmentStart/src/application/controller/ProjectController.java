4b3836d88f81fa38576dcb6907c1fd7d462d7eda
Hana1107 <87suzuki1107@gmail.com>
2023-10-14 17:11:14 -0700

    testing

--- TermProjectAssignmentStart/src/application/controller/ProjectController.java
+++ TermProjectAssignmentStart/src/application/controller/ProjectController.java
@@ -1,11 +1,18 @@
 package application.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 public class ProjectController {
 
     @FXML
@@ -30,4 +37,5 @@ public class ProjectController {
         MainController mainController = new MainController();
         mainController.redirectHelper(event, link);
     }
+    
 }
