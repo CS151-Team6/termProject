7f6197c44f8aef67c4ba6cd7cef95da43f4aff62
Hana1107 <87suzuki1107@gmail.com>
2023-11-08 16:40:39 -0800

    search project working in the main page

--- TermProjectAssignmentStart/src/application/controller/MainController.java
+++ TermProjectAssignmentStart/src/application/controller/MainController.java
@@ -13,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MainController implements Initializable {
@@ -21,6 +22,8 @@ public class MainController implements Initializable {
     private Hyperlink hyperlink;
     @FXML
     private ListView<String> projectList;
+    @FXML
+    private TextField keyword;
  
     @FXML
     private void homeToNewProject(ActionEvent event) {
@@ -98,6 +101,18 @@ public class MainController implements Initializable {
     	dbController.createProjectTable();
     	dbController.createTicketTable();
     	dbController.createCommentTable();
+    }
+    
+    //search for project
+    @FXML
+    public void searchProject(ActionEvent event) {
+    	DatabaseController dbc = new DatabaseController();
+    	if (keyword.getText() == null) {
+    		projectList.setItems(dbc.getAllProjects());
+    	} else {
+    		projectList.setItems(dbc.getProjectWithKeyword(keyword.getText()));
+    	}
+    	
     }
     
 
