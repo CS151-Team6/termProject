06555682042331564180818e9453e246a037f43a
Hana1107 <87suzuki1107@gmail.com>
2023-11-09 14:55:55 -0800

    the seach for ticket now working

--- TermProjectAssignmentStart/src/application/database/Ticket.java
+++ TermProjectAssignmentStart/src/application/database/Ticket.java
@@ -2,19 +2,29 @@ package application.database;
 
 public class Ticket {
 	private int id;
+	private String projId;
     private String name;
     private String created;
     private String description;
 
    
-    public Ticket(int id, String name, String created, String description) {
+    public Ticket(int id, String projId, String name, String created, String description) {
     	this.name = name;
         this.id = id;
         this.created = created;
         this.description = description;
+        this.projId = projId;
     }
     
-    public int getId() {
+    public String getProjId() {
+		return projId;
+	}
+
+	public void setProjId(String projId) {
+		this.projId = projId;
+	}
+
+	public int getId() {
     	return id;
     }
 
