41e4014035bc1156837b8d9add6bf7813416576d
Hana1107 <87suzuki1107@gmail.com>
2023-12-02 14:45:15 -0800

    Made it look pretty

--- TermProjectAssignmentStart/.metadata/.plugins/org.eclipse.core.resources/.history/f/10bde7700a76001e162aba359957ca34
+++
@@ -1,5 +0,0 @@
-module demo {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- termProject/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module termProject {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
